9b8e57f676cfe2434cf36e2bf8ce5132a020cd52
mark-torres10 <markptorres1@gmail.com>
2025-07-04 15:48:27 -0400

    remove babel

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   transform: {
-    '^.+\.(ts|tsx)$': 'babel-jest',
+    '^.+\.(ts|tsx)$': ['ts-jest', { tsconfig: 'tsconfig.jest.json' }],
   },
 };
+
